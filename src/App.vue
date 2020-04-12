7a8e33e606c0809b0ccb112d61cbadb015dde80d
Pablo <pquinteros@gmail.com>
2020-04-11 21:05:55 -0300

    Cree el maquetado y la lista

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todo :items=items />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Todo from './components/Todo.vue'
 
 export default {
   name: 'App',
+  data:function() {
+return {
+    items: [
+      { id:1, text: 'Leche' },
+      { id:2, text: 'Carne' },
+      { id:3, text: 'Huevos' }
+    ]
+  };
+},
   components: {
-    HelloWorld
+    Todo
   }
 }
+
+
 </script>
 
 <style>
+
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap");
+
+body{
+  font-family: "Inter", sans-serif;
+  background: #e4e9f3;
+  display: flex;
+  height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  text-align: left;
 }
 </style>
